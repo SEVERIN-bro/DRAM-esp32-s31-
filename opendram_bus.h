5bda4732acb41510958bff350c81d58570789eba
SEVERIN-bro <severin.kazeko@gmail.com>
2026-09-20 11:17:02 +0300

    Update the driver interface to the reduced-port shared DQ bus design

--- opendram_bus.h
+++ opendram_bus.h
@@ -7,27 +7,27 @@
 extern "C" {
 #endif
 
-#define OPENDRAM_WORD_COUNT 16384U
-#define OPENDRAM_ADDR_MASK  0x3FFFU
+#define OPENDRAM_ROW_BITS     14
+#define OPENDRAM_COL_BITS     13
+#define OPENDRAM_WORD_BITS    27
+#define OPENDRAM_MAX_ADDRESS  ((1UL << OPENDRAM_WORD_BITS) - 1)
+#define OPENDRAM_WORD_COUNT   (1UL << OPENDRAM_WORD_BITS)
 
 typedef struct {
     uint8_t addr_gpio[14];
-    uint8_t tx_gpio[16];
-    uint8_t rx_gpio[16];
+    uint8_t dq_gpio[16];
     uint8_t clk_gpio;
+    uint8_t cs_gpio;
+    uint8_t ras_gpio;
+    uint8_t cas_gpio;
+    uint8_t we_gpio;
 } opendram_bus_config_t;
 
 esp_err_t opendram_bus_init(const opendram_bus_config_t *cfg);
-
-esp_err_t opendram_bus_transfer(
-    uint16_t address,
-    uint16_t tx_data,
-    uint16_t *rx_data
-);
-
-uint16_t opendram_bus_read(uint16_t address);
-
-void opendram_bus_write(uint16_t address, uint16_t value);
+esp_err_t opendram_bus_write16(uint32_t address, uint16_t value);
+esp_err_t opendram_bus_read16(uint32_t address, uint16_t *value);
+uint32_t opendram_make_address(uint16_t row, uint16_t column);
+void opendram_decode_address(uint32_t address, uint16_t *row, uint16_t *column);
 
 #ifdef __cplusplus
 }
